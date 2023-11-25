da76912aa712444d2a28ad7f88ec5cfd0353bf98
Sumeda98 <ssbsankalpa@gmail.com>
2023-11-25 19:53:42 +0530

    change1

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -5,8 +5,8 @@ import HeroIMG_Mobile from "../../assets/images/HeroIMG_Mobile.svg";
 import { useTheme } from "@emotion/react";
 import Typewriter from "typewriter-effect";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import CallMadeIcon from '@mui/icons-material/CallMade';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import CallMadeIcon from "@mui/icons-material/CallMade";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const HeroSection = () => {
   const theme = useTheme();
@@ -14,49 +14,71 @@ const HeroSection = () => {
 
   return (
     <>
-      <Grid spacing={4} container sx={{ minHeight: isMobile ? '50vh':"100vh" }}>
+      <Grid
+        spacing={4}
+        container
+        sx={{ minHeight: isMobile ? "50vh" : "100vh" }}
+      >
         <Grid
           item
           xs={12}
           md={6}
           sx={{
-            mt:isMobile? 4:0,
+            mt: isMobile ? 4 : 0,
             display: "flex",
             alignItems: "center",
             justifyContent: { xs: "center", md: "left" },
           }}
         >
           <div>
-            <Typography variant="body1" color="text.iris" textAlign={isMobile ? 'center' :'left'}>
+            <Typography
+              variant="body1"
+              color="text.iris"
+              textAlign={isMobile ? "center" : "left"}
+            >
               Where your business takes flight.
             </Typography>
-            <Typography sx={{fontWeight:600}} textAlign={isMobile ? 'center' :'left'} variant={isMobile ? "h5" : "h4"}>
-              111We'll help you reach new heights in the{" "}
+            <Typography
+              sx={{ fontWeight: 600 }}
+              textAlign={isMobile ? "center" : "left"}
+              variant={isMobile ? "h5" : "h4"}
+            >
+              We'll help you reach new heights in the{" "}
               <span style={{ color: theme.palette.primary.main }}>
                 Digital World
               </span>
             </Typography>
-            <Typography textAlign={isMobile ? 'center' :'left'} variant="body2" color="text.secondary">
+            <Typography
+              textAlign={isMobile ? "center" : "left"}
+              variant="body2"
+              color="text.secondary"
+            >
               We'll amplify your brand's voice, resonating with audiences across
               the digital sphere.
             </Typography>
-            <div style={{marginTop:'20px', display:'flex', justifyContent:isMobile ? 'center':'left'}}>
-            <Button
-            sx={{mr:2, textTransform:'capitalize',p:1,px:2}}
-              variant="contained"
-              color="primary"
-              endIcon={<KeyboardArrowRightIcon />}
+            <div
+              style={{
+                marginTop: "20px",
+                display: "flex",
+                justifyContent: isMobile ? "center" : "left",
+              }}
             >
-              Start Your Project
-            </Button>
-            <Button
-            sx={{textTransform:'capitalize',p:1,px:2}}
-              variant="outlined"
-              color="primary"
-              endIcon={<CallMadeIcon />}
-            >
-              Request demo
-            </Button>
+              <Button
+                sx={{ mr: 2, textTransform: "capitalize", p: 1, px: 2 }}
+                variant="contained"
+                color="primary"
+                endIcon={<KeyboardArrowRightIcon />}
+              >
+                Start Your Project
+              </Button>
+              <Button
+                sx={{ textTransform: "capitalize", p: 1, px: 2 }}
+                variant="outlined"
+                color="primary"
+                endIcon={<CallMadeIcon />}
+              >
+                Request demo
+              </Button>
             </div>
           </div>
         </Grid>
@@ -71,7 +93,7 @@ const HeroSection = () => {
           }}
         >
           <img
-            src={isMobile? HeroIMG_Mobile:HeroIMG}
+            src={isMobile ? HeroIMG_Mobile : HeroIMG}
             alt="hero"
             style={{ maxWidth: "100%", height: "auto" }}
           />
